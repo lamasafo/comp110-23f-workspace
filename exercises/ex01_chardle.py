8d08f2ac6e376a06ac8c020e31a73b8b4d22cc4f
Laura Saavedra Forero <lsaavedra-forero@unc.com>
2023-09-17 01:37:09 -0400

    Progress for Exercise 1

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -5,7 +5,7 @@ word_guess: str = input("Enter a 5-character word: ")
 if len(word_guess) != 5:
     print("Error: Word must contain 5 characters")
     exit()
-letter_guess: str = input("Enter a single character:")
+letter_guess: str = input("Enter a single character: ")
 if len(letter_guess) != 1:
     print("Error: Character must be a single character.")
     exit()
@@ -13,19 +13,19 @@ letter_counter: int = 0
 print("Searching for " + letter_guess + "in" + word_guess)
 
 if letter_guess == word_guess[0]:
-    print(letter_guess + "found at index 0")
+    print(letter_guess + " found at index 0")
     letter_counter = letter_counter + 1
 if letter_guess == word_guess[1]:
-    print(letter_guess + "found at index 1")
+    print(letter_guess + " found at index 1")
     letter_counter = letter_counter + 1
 if letter_guess == word_guess[2]:
-    print(letter_guess + "found at index 2")
+    print(letter_guess + " found at index 2")
     letter_counter = letter_counter + 1
 if letter_guess == word_guess[3]:
-    print(letter_guess + "found at index 3")
+    print(letter_guess + " found at index 3")
     letter_counter = letter_counter + 1
 if letter_guess == word_guess[4]:
-    print(letter_guess + "found at index 4")
+    print(letter_guess + " found at index 4")
     letter_counter = letter_counter + 1
 
 if letter_counter == 1:
