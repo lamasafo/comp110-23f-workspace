281fefc9bf99a38a8b6f0dc314675e15fcd56554
Laura Saavedra Forero <lsaavedra-forero@unc.com>
2023-09-20 23:04:10 -0400

    Progress for Exercise 2

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -6,16 +6,12 @@ WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
 
-#Ask them to guess a 6-letter word
+# Ask them to guess a 6-letter word
 word_guess: str = str(input(f"What is your {len(secret_word)}-letter guess? "))
-#If word is not 6 letters, tell them
+# If word is not 6 letters, tell them
 while len(word_guess) != len(secret_word):
     print(f"That was not {len(secret_word)} letters! ")
-    word_guess: str = input("Try again: ")
-
-
-secret_index: int = 0
-guess_index: int = 0
+    word_guess != input("Try again: ")
 
 result_string: str = ""
 
@@ -31,16 +27,16 @@ while secret_index < len(secret_word):
             guess_index += 1 
         if check:
             result_string += YELLOW_BOX
-        else:
+        else: 
             result_string += WHITE_BOX
 
     secret_index += 1 
     
-#If guess word is correct, congratulate them.
+# If guess word is correct, congratulate them.
 if word_guess == secret_word:
     print(f"{result_string} ")
     print("Woo! You got it! ")
-#If word is not correct, tell them.
+# If word is not correct, tell them.
 else:
     print(f"{result_string}")
     print("Not quite. Play again soon! ")
