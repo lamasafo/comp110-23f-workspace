77bd82760d3f79a73c8cce31e1d620e1a6e7eeee
Laura Saavedra Forero <lsaavedra-forero@unc.com>
2023-09-20 23:20:27 -0400

    ex 2

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -11,7 +11,7 @@ word_guess: str = str(input(f"What is your {len(secret_word)}-letter guess? "))
 # If word is not 6 letters, tell them
 while len(word_guess) != len(secret_word):
     print(f"That was not {len(secret_word)} letters! ")
-    word_guess != input("Try again: ")
+    word_guess:str != input("Try again: ")
 
 
 secret_index: int = 0
