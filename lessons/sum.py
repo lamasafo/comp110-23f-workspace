a5582c49c5ef491ea6111fc0f92c60f534ab5211
Laura Saavedra Forero <lsaavedra-forero@unc.com>
2023-10-18 20:00:24 -0400

    progress

--- lessons/sum.py
+++ lessons/sum.py
@@ -2,8 +2,9 @@
 
 __author__ = "730478392"
 
-"""Summing elements using while loop."""
+
 def w_sum(vals: list[float]) -> float: 
+    """Summing elements using while loop."""
     current_sum: float = 0.0
     idx: int = 0
     while idx < len(vals): 
@@ -12,18 +13,16 @@ def w_sum(vals: list[float]) -> float:
     return current_sum 
 
 
-
-"""Summing elements using for loop."""
 def f_sum(vals: list[float]) -> float:
+    """Summing elements using for loop."""
     current_sum = 0.0
     for value in vals:
         current_sum += value
     return current_sum
     
 
-
-"""Summing elements using for loop and range."""
 def f_range_sum(vals: list[float]) -> float:
+    """Summing elements using for loop and range."""
     current_sum = 0.0
     for idx in range(len(vals)):
         current_sum += vals[idx]
